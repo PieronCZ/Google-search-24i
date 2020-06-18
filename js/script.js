148bbf3f13ecbe30738090802cd01c90622216bf
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-06-18 14:02:52 +0200

    Images, image as link

--- js/script.js
+++ js/script.js
@@ -5,50 +5,65 @@ const search = document.getElementById("search"),
   resultTxtHeading = document.getElementById("result-txt-heading"),
   imagesEl = document.getElementById("images"),
   resultsEl = document.getElementById("results"),
-  container = document.getElementsByClassName('.container');
+  container = document.getElementById("container");
 
-// Search for keyword and fetch from API
+  // Search for keyword and fetch from API
 function searching(e) {
   e.preventDefault();
-  imagesEl.innerHTML = "";
 
   const myQuery = search.value;
   if (myQuery.trim()) {
-      try{
-         fetch( `https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&q=${myQuery}`)
-            .then((res) => res.json()) // catch promise
-            .then((data) => {   // catching second promise
-                console.log(data);
-                resultImgHeading.innerHTML = `<h2>Image result for: ${myQuery}</h2>`;
-                resultTxtHeading.innerHTML = `<h2>Web result for: ${myQuery}</h2>`;
-                if(!data.items){
-                    container.innerHTML = `
-                        <h3>It seems there is nothink you are looking for...</h3>
-                    `;
-                }else{
-                    resultsEl.innerHTML = data.items.map(item =>
-                        `<h3><a href="${item.link}">${item.htmlTitle}</a></h3>
-                        <p><a href="${item.link}">${item.link}</a></p>
-                        <p>${item.htmlSnippet}</p>
-                        `
-                    ).join('');
-
-                    // imagesEl.innerHTML = data.items.map(img => 
-                    //     {if(img.image){
-                    //        `<img src="${img.image}" />
-                    //     ` 
-                    //     }else
-                    //     ``
-                    //     }).join('');
-                }
-            }); 
-        }catch(ex){
-            console.log(ex);
-        }
-    
+    webResults(myQuery);
+    imageResults(myQuery);
   } else {
     console.log("type smth");
   }
+}
+
+function webResults(myQuery){
+  try {
+    fetch(
+      `https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&q=${myQuery}`
+    )
+      .then((res) => res.json()) // catch promise
+      .then((data) => { // catching second promise
+        console.log(data);
+        resultTxtHeading.innerHTML = `<h2>Web result for: ${myQuery}</h2>`;
+        if (!data.items) {
+          container.innerHTML = `<h3>It seems there is nothink you are looking for...</h3>`;
+        } else {
+          resultsEl.innerHTML = data.items
+            .map((item) =>
+                `<h3><a href="${item.link}">${item.htmlTitle}</a></h3>
+                      <p><a href="${item.link}">${item.link}</a></p>
+                      <p>${item.htmlSnippet}</p>`
+            ).join("");
+        }
+      });
+  } catch (ex) {
+    console.log(ex);
+  }
+}
+
+function imageResults(myQuery){
+  try{
+    fetch(
+      `https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=9&searchType=image&q=${myQuery}`
+    )
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      resultImgHeading.innerHTML = `<h2>Image result for: ${myQuery}</h2>`;
+      if(!data.items){
+         container.innerHTML =  `<h3>It seems there is nothink you are looking for...</h3>`;
+      }else{
+        imagesEl.innerHTML = data.items.map(img => `<a href="${img.image.contextLink}"><img src="${img.link}" /></a>`).join('');
+      }
+    })
+  }catch(ex){
+    console.log(ex);
+  }
+  
 }
 
 // Event listeners
