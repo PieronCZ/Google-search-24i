2cf648be335a30fc925bd796da638b1e63a6f43e
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-06-19 22:59:10 +0200

    No connection functionality done

--- js/script.js
+++ js/script.js
@@ -16,13 +16,11 @@ var webUrl;
 function searching(e) {
   e.preventDefault();
   const myQuery = search.value;
-  
-  if(!myQuery.trim()) 
-  {
+
+  if (!myQuery.trim()) {
     console.log("nothing to search");
-  } 
-  else {
-    doesConnectionExist()
+  } else {
+    doesConnectionExist();
     webUrl = `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&q=${myQuery}`;
     webResults(myQuery, webUrl);
     imageResults(myQuery);
@@ -33,7 +31,8 @@ function webResults(myQuery, url) {
   container.style.display = "grid";
   fetch(url)
     .then((res) => res.json()) // catch promise
-    .then((data) => { // catching second promise
+    .then((data) => {
+      // catching second promise
       console.log("fetch returned");
       console.log(data);
       resultTxtHeading.innerHTML = `<h2>Web result for: ${myQuery}</h2>`;
@@ -52,10 +51,14 @@ function webResults(myQuery, url) {
         // check if there is next page - pagination ${previous > 0 ? `<button onclick="getMoreResults('${previous}','${myQuery}')">Prev</button>` : ''}
         if (data.queries.nextPage) {
           let startIndex = data.queries.nextPage.startIndex;
-          let previous = startIndex-4;
+          let previous = startIndex - 4;
           pagination.innerHTML = `
             
-            ${data.queries.nextPage ? `<button onclick="getMoreResults('${startIndex}','${myQuery}')">Next</button>` : ''}`;
+            ${
+              data.queries.nextPage
+                ? `<button onclick="getMoreResults('${startIndex}','${myQuery}')">Next</button>`
+                : ""
+            }`;
         } else {
           pagination.innerHTML = "";
         }
@@ -95,34 +98,32 @@ function imageResults(myQuery) {
   console.log("finish");
 }
 
-
 // Check internet connection
 function doesConnectionExist() {
-    var xhr = new XMLHttpRequest();
-    var file = "https://pieroncz.github.io/website/";
-    var randomNum = Math.round(Math.random() * 10000);
- 
-    xhr.open('HEAD', file + "?rand=" + randomNum, true);
-     console.log(xhr.response); 
-    xhr.send();
-     
-   
-    xhr.addEventListener("readystatechange", processRequest, false);
- 
-    function processRequest(e) {
-      if (xhr.readyState == 4) {
-        if (xhr.status >= 200 && xhr.status < 304) {
-          console.log("connection exists!");
-          return true;
-        } else {
-          console.log("connection doesn't exist!");
-            popup.style.display = 'flex';
-           return false;
-        }
+  var xhr = new XMLHttpRequest();
+  var file = "https://pieroncz.github.io/website/"; // Link to hosted random project on my Github which I am sure exist
+  var randomNum = Math.round(Math.random() * 10000);
+
+  xhr.open("HEAD", file + "?rand=" + randomNum, true);
+  console.log(xhr.response);
+  xhr.send();
+
+  xhr.addEventListener("readystatechange", processRequest, false);
+
+  function processRequest(e) {
+    if (xhr.readyState == 4) {
+      if (xhr.status >= 200 && xhr.status < 304) {
+        // out of that range something is going wrong
+        console.log("connection exists!");
+        return true;
+      } else {
+        console.log("connection doesn't exist!");
+        popup.style.display = "flex";
+        return false;
       }
     }
+  }
 }
-
 
 // Event listeners
 submit.addEventListener("submit", searching);
