851df78b523d9d9ad3b2dc0dbde4fe8112eb2191
Mateusz Brzoska <mateuszbrzoska@seznam.cz>
2020-06-19 17:15:57 +0200

    Pagination, internet status checking idea

--- js/script.js
+++ js/script.js
@@ -5,71 +5,134 @@ const search = document.getElementById("search"),
   resultTxtHeading = document.getElementById("result-txt-heading"),
   imagesEl = document.getElementById("images"),
   resultsEl = document.getElementById("results"),
+  retryBtn = document.getElementById("retry"),
+  popup = document.getElementById("popup-container"),
+  pagination = document.getElementById("pagination"),
   container = document.getElementById("container");
-  pagination = document.getElementById('pagination');
 
-  // Search for keyword and fetch from API
+var webUrl;
+
+// Search for keyword and fetch from API
 function searching(e) {
   e.preventDefault();
-
   const myQuery = search.value;
+  /*
+  if(!myQuery.trim()) 
+  {
+    console.log("nothing to search");
+  } 
+  else if(!doesConnectionExist())
+  {
+    popup.style.display = 'flex';
+  }*/
   if (myQuery.trim()) {
-    webResults(myQuery);
+    //&& doesConnectionExist())
+    webUrl = `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&q=${myQuery}`;
+    webResults(myQuery, webUrl);
     imageResults(myQuery);
   } else {
-    console.log("type smth");
+    console.log("unknown error");
   }
 }
 
-function webResults(myQuery){
-  try {
-    fetch(
-      `https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&q=${myQuery}`
-    )
-      .then((res) => res.json()) // catch promise
-      .then((data) => { // catching second promise
-        console.log(data);
-        resultTxtHeading.innerHTML = `<h2>Web result for: ${myQuery}</h2>`;
-        if (!data.items) {
-          container.innerHTML = `<h3>It seems there is nothink you are looking for...</h3>`;
-        } else {
-          resultsEl.innerHTML = data.items
-            .map((item) =>
-                `<h3><a href="${item.link}">${item.htmlTitle}</a></h3>
-                      <p><a href="${item.link}">${item.link}</a></p>
-                      <p>${item.htmlSnippet}</p>`
-            ).join("");
-            let nextResIndex = toString(data.queries.nextPage.startIndex);
-            console.log(nextResIndex);
-            // pagination.innerHTML = `<li class="page-item"><a class="page-link" href="https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&q=${myQuery}">Previous</a></li>
-            //     <li class="page-item"><a class="page-link" href="">Next</a></li>`
-        }
-      });
-  } catch (ex) {
-    console.log(ex);
-  }
-}
-
-function imageResults(myQuery){
-  try{
-    fetch(
-      `https://www.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=9&searchType=image&q=${myQuery}`
-    )
-    .then(res => res.json())
-    .then(data => {
+function webResults(myQuery, url) {
+  container.style.display = "grid";
+  fetch(url)
+    .then((res) => res.json()) // catch promise
+    .then((data) => {
+      // catching second promise
+      console.log("fetch returned");
       console.log(data);
-      resultImgHeading.innerHTML = `<h2>Image result for: ${myQuery}</h2>`;
-      if(!data.items){
-         container.innerHTML =  `<h3>It seems there is nothink you are looking for...</h3>`;
-      }else{
-        imagesEl.innerHTML = data.items.map(img => `<a href="${img.image.contextLink}"><img src="${img.link}" /></a>`).join('');
+      resultTxtHeading.innerHTML = `<h2>Web result for: ${myQuery}</h2>`;
+      if (!data.items) {
+        container.innerHTML = `<h3>We couldn't find any results under: ${myQuery}</h3>`;
+      } else {
+        resultsEl.innerHTML = data.items
+          .map(
+            (item) =>
+              `<h3><a href="${item.link}">${item.htmlTitle}</a></h3>
+                            <p><a href="${item.link}">${item.link}</a></p>
+                            <p>${item.htmlSnippet}</p>`
+          )
+          .join("");
+
+        // check if there is next page - pagination
+        if (data.queries.nextPage) {
+          let startIndex = data.queries.nextPage.startIndex;
+          let previous = startIndex-4;
+          let query = myQuery;
+          pagination.innerHTML = `
+            ${previous > 0 ? `<button onclick="getMoreResults('${previous}','${query}')">Prev</button>` : ''}
+            ${data.queries.nextPage ? `<button onclick="getMoreResults('${startIndex}','${query}')">Next</button>` : ""}`;
+        } else {
+          pagination.innerHTML = "";
+        }
       }
     })
-  }catch(ex){
-    console.log(ex);
-  }
-  
+    .catch((reason) => console.log(`Fetch failed because ${reason}`));
+  console.log("finish");
 }
 
+function getMoreResults(startIndex, query) {
+  webUrl = `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=4&start=${startIndex}&q=${query}`;
+  webResults(query, webUrl);
+}
+
+function imageResults(myQuery) {
+  container.style.display = "grid";
+  fetch(
+    `https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=9&searchType=image&q=${myQuery}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("fetch returned");
+      console.log(data);
+      resultImgHeading.innerHTML = `<h2>Image result for: ${myQuery}</h2>`;
+      if (!data.items) {
+        container.innerHTML = `<h3>We did not find any results</h3>`;
+      } else {
+        imagesEl.innerHTML = data.items
+          .map(
+            (img) =>
+              `<a href="${img.image.contextLink}"><img src="${img.link}" /></a>`
+          )
+          .join("");
+      }
+    })
+    .catch((reason) => console.log(`Fetch failed because ${reason}`));
+  console.log("finish");
+}
+
+/*
+// Check internet connection
+function doesConnectionExist() {
+    var xhr = new XMLHttpRequest();
+  
+    var file = "https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyA1EmO9ayEkJ3V7Fd5D5nbI2BLby45eeMk&cx=014821957775912081400:skraxelfrf0&num=9&searchType=image&q=$steak";
+    
+    var randomNum = Math.round(Math.random() * 10000);
+
+    
+    xhr.open('HEAD', file + "?rand=" + randomNum, 'Origin', 'http://127.0.0.1:5501/index.html', true);
+    xhr.send();
+     
+    xhr.addEventListener("readystatechange", processRequest, false);
+ 
+    function processRequest(e) {
+      if (xhr.readyState == 4) {
+        if (xhr.status >= 200 && xhr.status < 304) {
+          return true;
+        } else {
+          popup.style.display = 'flex';
+          return false;
+        }
+      }
+    }
+};
+*/
 // Event listeners
 submit.addEventListener("submit", searching);
+retryBtn.addEventListener("click", (e) => {
+  searching(e);
+  popup.style.display = "none";
+});
